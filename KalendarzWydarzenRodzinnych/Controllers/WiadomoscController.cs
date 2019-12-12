cca989cbcf90d0213ada426d04148e0dd38d1810
xxmati96xx <mati10_96.1996@o2.pl>
2019-12-12 23:46:58 +0100

    Dodawanie nowej wiadomości. Możliwość usunięcia lokalnie dla konta a jeśli nadawca i odbiorca usuną wiadomość to usuwa się content wiadomości

--- KalendarzWydarzenRodzinnych/Controllers/WiadomoscController.cs
+++ KalendarzWydarzenRodzinnych/Controllers/WiadomoscController.cs
@@ -18,7 +18,7 @@ namespace KalendarzWydarzenRodzinnych.Controllers
         public ActionResult ListSend()
         {
             var idUzytkownik = Convert.ToInt32(User.Identity.GetUzytkownikId());
-            IEnumerable<Wyslane_Wiadomosc> send = dbo.Wyslane_Wiadomosc.Include(w=>w.Uzytkownik).Include(w => w.Wiadomosc).Where(w => w.Od == idUzytkownik);
+            IEnumerable<Wyslane_Wiadomosc> send = dbo.Wyslane_Wiadomosc.Include(w=>w.Uzytkownik).Include(w => w.Wiadomosc).Where(w => w.Od == idUzytkownik).OrderByDescending(w=>w.id);
             return View(send.ToList()); ;
         }
 
@@ -30,20 +30,125 @@ namespace KalendarzWydarzenRodzinnych.Controllers
         [HttpGet]
         public ActionResult SendMessage()
         {
+            Message message = new Message();
+            message.UserColection = dbo.Uzytkownik.ToList();
             var users = dbo.Uzytkownik
                 .Select(u => new
                 {
                     id = u.id,
                     user = u.Imie + " " + u.Nazwisko
                 }).ToList();
-            ViewBag.Do = new MultiSelectList(users, "id", "user");
-            return View();
+            ViewBag.Do = users;
+            return View(message);
         }
 
         [HttpPost]
         public ActionResult SendMessage(Message message)
         {
-            return View();
+            if(message == null)
+            {
+                return HttpNotFound();
+            }
+            try
+            {
+                Wiadomosc wiadomosc = message.wiadomosc;
+                dbo.Wiadomosc.Add(wiadomosc);
+                dbo.SaveChanges();
+                if (message.SelectedID.Length != 0)
+                    foreach (var item in message.SelectedID)
+                    {
+                        Wyslane_Wiadomosc send_message = new Wyslane_Wiadomosc();
+                        send_message.id_wiadomosc = wiadomosc.id;
+                        send_message.Od = Convert.ToInt32(User.Identity.GetUzytkownikId());
+                        send_message.Do = item;
+                        dbo.Wyslane_Wiadomosc.Add(send_message);
+
+                        Odebrane_Wiadomosc received_message = new Odebrane_Wiadomosc(); //zmiana na nie dynamiczne id i branie go z send_message zabezpieczenie przed ewentualnymi błedami przy jednoczesnym zapisie do bazy z wielu różnych miejsc
+                        received_message.id_wiadomosc = wiadomosc.id;
+                        received_message.Od = Convert.ToInt32(User.Identity.GetUzytkownikId());
+                        received_message.Do = item;
+                        dbo.Odebrane_Wiadomosc.Add(received_message);
+                        dbo.SaveChanges();
+                    }
+            }catch(Exception ex)
+            {
+
+            }
+            return RedirectToAction("ListSend");
         }
+
+        public ActionResult Details(int? id,string r)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            if(r == "s")
+            {
+                Wyslane_Wiadomosc send_message = dbo.Wyslane_Wiadomosc.Include(s=>s.Wiadomosc).Include(s => s.Uzytkownik1).Include(s => s.Uzytkownik).Where(s => s.id == id).FirstOrDefault(); 
+               
+                if (send_message == null)
+                {
+                    return HttpNotFound();
+                }
+                return View("DetailsSend",send_message);
+            }
+            else
+            {
+                Odebrane_Wiadomosc received_message = dbo.Odebrane_Wiadomosc.Include(s => s.Wiadomosc).Include(s => s.Uzytkownik1).Include(s => s.Uzytkownik).Where(s => s.id == id).FirstOrDefault();
+                if (received_message == null)
+                {
+                    return HttpNotFound();
+                }
+                return View("DetailsReceived",received_message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteMessage(int? id, string r)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if(r == "s") {
+                Wyslane_Wiadomosc send_message = dbo.Wyslane_Wiadomosc.Find(id);
+                if (send_message == null)
+                {
+                    return HttpNotFound();
+                }
+            
+            return View("DeleteMessageSend",send_message);
+            }
+            else
+            {
+                Odebrane_Wiadomosc received_message = dbo.Odebrane_Wiadomosc.Find(id);
+                if (received_message == null)
+                {
+                    return HttpNotFound();
+                }
+
+            return View("DeleteMessageRecived", received_message);
+            }
+
+
+        }
+
+        [HttpPost]
+        public ActionResult DeleteMessageSend(int id)
+        {
+            Odebrane_Wiadomosc received_message = dbo.Odebrane_Wiadomosc.Find(id);
+            if (received_message == null)
+            {
+                Wyslane_Wiadomosc send_message = dbo.Wyslane_Wiadomosc.Find(id);
+                Wiadomosc message = dbo.Wiadomosc.Find(send_message.id_wiadomosc);
+                dbo.Wyslane_Wiadomosc.Remove(send_message);
+                dbo.Wiadomosc.Remove(message);
+                dbo.SaveChanges();
+            }
+            
+            return RedirectToAction("ListSend");
+        }
+    
     }
 }
--- KalendarzWydarzenRodzinnych/Models/Message.cs
+++ KalendarzWydarzenRodzinnych/Models/Message.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KalendarzWydarzenRodzinnych.Models
 {
     public class Message
     {
         public Wiadomosc wiadomosc { get; set; }
+
         public int Do { get; set; }
+
+        [NotMapped]
+        public IEnumerable<Uzytkownik> UserColection { get; set; }
+
+        [NotMapped]
+        public int[] SelectedID { get; set; }
     }
 }
