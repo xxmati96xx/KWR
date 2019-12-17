54190bba43ba742c1c9be56d35a99907e5cd408a
xxmati96xx <mati10_96.1996@o2.pl>
2019-12-18 00:27:55 +0100

    Drobne poprawki przy pisaniu pracy dyplomowej

--- KalendarzWydarzenRodzinnych/Models/Powiadomienie_Wyswietl_Result.cs
+++ KalendarzWydarzenRodzinnych/Models/Powiadomienie_Wyswietl_Result.cs
@@ -10,14 +10,20 @@
 namespace KalendarzWydarzenRodzinnych.Models
 {
     using System;
-    
+    using System.ComponentModel.DataAnnotations;
     public partial class Powiadomienie_Wyswietl_Result
     {
         public Nullable<int> id { get; set; }
+        [Display(Name = "Data przypomnienia")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
         public System.DateTime DataPowiadomienia { get; set; }
+        [Display(Name = "Treœæ")]
         public string Tresc { get; set; }
+        [Display(Name = "Nazwa wydarzenia")]
         public string Tytul { get; set; }
         public string identyfier { get; set; }
+        [Display(Name = "Rodzaj przypomnienia")]
         public string rodzaj { get; set; }
     }
 }
